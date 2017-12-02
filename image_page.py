7748d052752ef599c0df1ea98ffddd7ea9fbea38
kenwang815 <starwish75815@gmail.com>
2017-12-02 23:19:30 +0800

    add running information

--- image_page.py
+++ image_page.py
@@ -60,7 +60,6 @@ def _get_image_link(url):
 
 
 def _download(path, url):
-    print("path={0} url={1}".format(path, url))
     r = requests.get(url, stream=True)
     if r.status_code == 200:
         with open(path, 'wb') as f:
@@ -71,19 +70,25 @@ def _download(path, url):
 def collect_image(category_info):
     all_img_dataset = set()
 
-    print(category_info)
     page_links = _get_all_page_link(category_info['link'])
-    for page, link in enumerate(page_links):
-        print("page={0} link={1}".format(page, link))
+    page_count = page_links.__len__()
+    print("collect all image link")
+    for index, link in enumerate(page_links):
+        print("image page={0}/{1} link={2}".format(index + 1, page_count, link))
         all_img_dataset.update(_get_image_link(link))
-        time.sleep(1)
+#        time.sleep(1)
+    print("\n")
 
     save_path = "{0}/{1}".format(category_info['page_path'], category_info['name'])
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
+    print("save image in {0}".format(save_path))
+    img_count = all_img_dataset.__len__()
     for index, url in enumerate(all_img_dataset):
         extension = url.split(".")[-1]
         img_path = "{0}/{1}.{2}".format(save_path, index, extension)
+        print("image={0}/{1} from={2} destination={3}".format(index + 1, img_count, url, img_path))
         _download(img_path, url)
-        time.sleep(2)
+#        time.sleep(2)
+    print("\n")
--- main.py
+++ main.py
@@ -14,16 +14,19 @@ if __name__ == "__main__":
 
     page_links = main_page.get_all_page_link()
 
+    page_count = page_links.__len__()
     for page, link in enumerate(page_links):
-        print("page={0} link={1}".format(page, link))
+        print("main page={0}/{1} link={2}\n".format(page + 1, page_count, link))
         html = urlopen(link)
         bs_obj = BeautifulSoup(html, "html.parser")
         category_list = bs_obj.find_all("div", {"class": "grid"})
 
         page_path = "{0}/{1}".format(config.download_folder, page)
-        for category in category_list:
+        category_count = category_list.__len__()
+        for index, category in enumerate(category_list):
             category_info = main_page.parser_category(page, category, page_path)
+            print("category={0}/{1} name={2}\n".format(index + 1, category_count, category_info["name"]))
             image_page.collect_image(category_info)
 
-        time.sleep(5)
+#        time.sleep(5)
         print("\n")

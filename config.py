85dfeb02b821ab813c2909a8c53885db53dd902b
kenwang815 <starwish75815@gmail.com>
2017-12-03 15:26:11 +0800

    add command line function

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # -*- coding: UTF-8 -*-
-
+export_folder = "src"
 download_folder = "images"
 url = "http://rthost.cr.rs/sd/"
--- main.py
+++ main.py
@@ -6,12 +6,21 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import os
 import time
+import click
+import json
+import configparser
 
 
-if __name__ == "__main__":
+@click.group()
+def cli():
+    if not os.path.exists(config.export_folder):
+        os.makedirs(config.export_folder)
     if not os.path.exists(config.download_folder):
         os.makedirs(config.download_folder)
 
+
+@click.command()
+def start():
     page_links = main_page.get_all_page_link()
 
     page_count = page_links.__len__()
@@ -27,6 +36,45 @@ if __name__ == "__main__":
             category_info = main_page.parser_category(page, category, page_path)
             print("category={0}/{1} name={2}\n".format(index + 1, category_count, category_info["name"]))
             image_page.collect_image(category_info)
+            # time.sleep(5)
 
-#        time.sleep(5)
         print("\n")
+
+
+@click.command()
+def pop():
+    config_parser = configparser.RawConfigParser()
+    page_links = main_page.get_all_page_link()
+
+    config_parser.add_section("Page")
+    for index, link in enumerate(page_links):
+        config_parser.set("Page", str(index), link)
+
+    config_parser.add_section("Other")
+    config_parser.set("Other", "Count", len(page_links))
+
+    config_parser.add_section("Download")
+    config_parser.set("Download", "Timestamp", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+    save_path = "{0}/main_page.cfg".format(config.export_folder)
+    with open(save_path, 'w') as configfile:
+        config_parser.write(configfile)
+
+
+@click.command()
+def push():
+    config_parser = configparser.ConfigParser()
+    config_parser.read("{0}/main_page.cfg".format(config.export_folder))
+
+    count = int(config_parser.get("Other", "Count"))
+    for index in range(0, count):
+        print(config_parser.get("Page", str(index)))
+
+
+cli.add_command(start)
+cli.add_command(pop)
+cli.add_command(push)
+
+
+if __name__ == "__main__":
+    cli()
